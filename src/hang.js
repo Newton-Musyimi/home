48c28a40ba95c47690c545c8c4611431c7f4b6c3
Musyimi <newtonthemaker@gmail.com>
2020-04-05 16:18:26 +0200

    hello

---
+++ src/hang.js
@@ -0,0 +1,39 @@
+var words = ["Flabbergasted", "Perplexed", "Revered", "Buoyant"];
+let prom = true;
+//let prom = confirm("Do you want to play Hangman");
+if (prom) {
+    var word = words[Math.floor(Math.random() * 4)];
+    //alert(word);
+    console.log(word);
+    var remove = Math.floor(word.length/2);
+    console.log("to remove is " + remove)
+    var pluck = [];
+    var wordArray = word.split("");
+    //alert(wordArray);
+    console.log(wordArray);
+    console.log("Word length is "+word.length)
+    for(let i = 0;i<remove;++i){
+        var num = Math.floor(Math.random() * word.length);
+        if(pluck.indexOf(num) === -1){
+            pluck += num;
+            
+        }else{
+            while(pluck.indexOf(num) !== -1){
+                num = Math.floor(Math.random()*word.length);
+            }
+            pluck += num;
+        }
+        //alert(pluck);
+        console.log(pluck);
+    }
+    for(let j = 0;j<pluck.length;++j){
+        var index = pluck[j];
+        wordArray[index] = "_";
+    }
+    //alert(wordArray);
+    console.log(wordArray.join(""));
+    
+}else{
+    alert("Boo!");
+}
+
